c2d2964f7b1d96711508b569433513887c9de657
VitorFerreira98 <vitorsantos981@outlook.com>
2023-11-28 20:50:50 -0300

    Initial commit

---
+++ Desafios/Desafio 06/node_5b14e78b1f26b.tmp.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio-06</title>
+</head>
+<body>
+    <h1>Tags em HTML!</h1>
+    <p>Em HTML5, podemos inserir os elemoentos da lista a seguir. Passe o mouse sobre os termos para descobrir qual tag deverá utilizar em cada caso.</p>
+    <ul >
+        <li><abbr title="Use a tag<p>">Parágrafos</abbr></li>
+        <li><abbr title="#">Títulos</abbr></li>
+        <li><abbr title="#">Imagens</abbr></li>
+        <li><abbr title="#">Abreviações</abbr></li>
+        <li><abbr title="#">Códigos-fonte</abbr></li>
+        <li>Listas numeradas</li>
+        <li>Listas com demarcações</li>
+    </ul>
+
+    
+</body>
+</html>
---
+++ Desafios/Desafio 10 - (Projeto Android)/README.md
@@ -0,0 +1,2 @@
+# ProjetoAndroid
+ ProjetoAndroid
